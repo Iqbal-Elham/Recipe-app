a4ec2fbf6b14f1399b2a6bae0ec1e06e0786014e
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-07-03 22:00:57 +0430

    Add foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  resources :foods
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
-  
+
   devise_scope :user do
     get '/users/logout', to: 'devise/sessions#destroy', as: :logout
   end
   devise_for :users
   resources :recipes, except: %i[edit update]
-
+  resources :foods, except: %i[show edit update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_155033) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_161311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.integer "quantity"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name", null: false
@@ -32,10 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_155033) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
@@ -43,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_155033) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
 end
