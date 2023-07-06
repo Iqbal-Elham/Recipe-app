d6106e1788f10083e129cc71b52ec9da1c3aed88
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-07-06 23:59:10 +0430

    root_path problem

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root 'recipes#public_recipes'
   devise_for :users
   resources :recipes
   get '/recipe_visibility/:id', to: 'recipes#toggle_visibility', as: :visibility
   get '/shopping_list', to: 'recipes#shopping_list', as: :shopping_list
   resources :foods, except: %i[show edit update]
-
 end
