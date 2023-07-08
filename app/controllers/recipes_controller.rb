fca2e8ce2c683a842461582b37ebcd594e0e5372
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-07-08 14:27:16 +0430

    Added requested changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -43,12 +43,27 @@ class RecipesController < ApplicationController
     @total_value = 0
     @recipes.each do |recipe|
       recipe.recipes_foods.each do |recipes_food|
-        unless @foods.include?(recipes_food.food)
+        puts '======================================'
+        puts recipes_food
+        puts '+++++++++++++++++++++++++++++++++++++++'
+        puts recipes_food.quantity
+        puts '+++++++++++++++++++++++++++++++++++++++'
+        puts recipes_food.food
+        puts '+++++++++++++++++++++++++++++++++++++++'
+        puts recipes_food.food.quantity
+        puts '======================================'
+        if !@foods.include?(recipes_food.food)
           @missing_foods << recipes_food
           @total_value += (recipes_food.food.price * recipes_food.quantity)
+          puts 
+          p @missing_foods
+        elsif (recipes_food.quantity - recipes_food.food.quantity) > 0
+          @missing_foods << recipes_food
+          @total_value += (recipes_food.food.price * (recipes_food.quantity - recipes_food.food.quantity))
         end
       end
     end
+    puts @total_value
     render 'shopping_list'
   end
 
